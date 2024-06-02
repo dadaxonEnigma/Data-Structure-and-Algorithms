eb023346d7d616ada85ef9a77cbda120c76f556e
dadaxonEnigma <voiceofenigma19@gmail.com>
2024-06-02 23:27:36 +0300

    Linked List (print, append, insert at start, get length) methods

--- linked_list.py
+++ linked_list.py
@@ -1,64 +1,53 @@
-# class Node:
-#     def __init__(self, data=None,next=None):
-#         self.data = data
-#         self.next = next
-# class LinkedList:
-#     def __init__(self) -> None:
-#         self.head = None
-#     def insert_at_begining(self,data):
-#         node = Node(data,self.head)
-#         self.head = node
-#     def print(self):
-#         if self.head is None:
-#             print("Linked lis ist empty")
-#             return
-#         itr = self.head
-#         llstr = ''
-#         while itr:
-#             llstr += str(itr.data) + '-->'
-#             itr = itr.next
-#         print(llstr)
-    
-#     def insert_at_end(self,data):
-#         if self.head is None:
-#             self.head = Node(data,None)
-#             return
-
-#         itr = self.head
-#         while itr.next:
-#             itr = itr.next
-        
-#         itr.next = Node(data, None)
-
-# if __name__ == '__main__':
-#     ll = LinkedList()
-#     ll.insert_at_end(79)
-#     ll.insert_at_end(1)
-#     ll.insert_at_end(349)
-#     ll.insert_at_begining(3)
-    
-#     ll.print()
-
 class Node:
-    def __init__(self,data):
+    def __init__(self,data = None, next = None) -> None:
         self.data = data
-        self.next = None
-    
-    def __str__(self):
-        return f"[{self.data}]->{self.next}"
+        self.next = next
 
 class LinkedList:
     def __init__(self) -> None:
         self.head = None
+    
+    def insert_at_start(self,data):
+        node = Node(data,self.head)
+        self.head = node
         
-    def __str__(self) -> str:
-        return str(self.head)
+    def insert_at_end(self,data):
+        if self.head is None:
+            self.head = Node(data,None)
+            return
+        cur_node = self.head
+        while cur_node.next:
+            cur_node = cur_node.next
+        cur_node.next = Node(data,None)
+    
+    def print_list(self):
+        if self.head is None:
+            print('Linked List is empty')
+            return
+        cur_node = self.head
+        llstr = ''
+        while cur_node:
+            llstr += str(cur_node.data) + '-->'
+            cur_node = cur_node.next
+        print(llstr)
+        
+    def insert_values(self,data_list):
+        self.head = None
+        for data in data_list:
+            self.insert_at_end(data)
 
-if __name__ == '__main__':
-    linked_list = LinkedList()
-    temp = Node(1)
-    linked_list.head = temp
-    for i in range(2,9):
-        temp.next  = Node(i)
-        temp = temp.next
-    print(linked_list)
+    def get_length(self):
+        count = 0
+        cur_node = self.head
+        while cur_node:
+            count += 1
+            cur_node = cur_node.next
+        return count
+
+llist = LinkedList()
+llist.insert_at_start('1')
+llist.insert_at_start('2')
+llist.insert_at_end('5')
+llist.insert_values([2,4,5,5,6,6,6])
+print(llist.get_length())
+llist.print_list()
