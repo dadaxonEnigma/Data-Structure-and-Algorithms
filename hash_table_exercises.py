a32858e199054520dad2866652b0cef4227bbda8
dadaxonEnigma <voiceofenigma19@gmail.com>
2024-06-07 23:49:22 +0300

    Hash Table first exercises

---
+++ hash_table_exercises.py
@@ -0,0 +1,14 @@
+# nyc_weather.csv contains new york city weather for first few days in the month of January. Write a program that can answer following,
+# What was the average temperature in first week of Jan
+# What was the maximum temperature in first 10 days of Jan
+# Figure out data structure that is best for this problem
+arr = []
+
+with open("nyc_weather.csv","r") as f:
+    for line in f:
+        tokens = line.split(',')
+        try:
+            temperature = int(tokens[1])
+            arr.append(temperature)
+        except:
+            print("Invalid temperature.Ignore the row")
