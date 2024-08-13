2a15fefc13dc759ecc1103725ab80737ee10566c
dadaxonEnigma <voiceofenigma19@gmail.com>
2024-08-13 20:24:15 +0300

    Task 2

--- Task_2.py
+++ Task_2.py
@@ -1,7 +1,28 @@
-def mysum(*numbers):
+# 1. Функция принимает один аргумент
+def my_numbs_first(*args):
     output = 0
-    for number in numbers:
-        output += number
+    for numb in args:
+        output += numb
     return output
 
-print(mysum(*(10,20,40,55)))
+print(my_numbs_first(1,2,3,4,5,6))
+
+# 2. Функция принимает два аргумента
+def my_numbs_second(*args, start = 0):
+    output = 0
+    for numb in args:
+        output += numb
+    return output + start
+
+print(my_numbs_second(*[1,2,3,4,5],5))
+
+# 3.  Напишите функцию, которая принимает список чисел. Он
+# должен вернуть среднее значение (т.е. среднее арифметическое) этих чисел
+
+def my_numb(*args):
+    std = 0
+    for numb in args:
+        std += numb 
+    return int(std / len(args))
+
+print(my_numb(*[1,2,3,4,5]))
